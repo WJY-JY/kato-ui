5464e3e31baaf0bc796e6aaf431f7a8f574f893e
王加雨 <wangjy@hiysg.com>
2019-01-04 18:35:11 +0800

    添加各种类型测试

--- example/test.js
+++ example/test.js
@@ -5,15 +5,18 @@ export default class Test {
   @validate(should.object({
       k1: should.string().example("example1").description("测试yi"),
       k2: should.number().integer().example(1).required().description("测试"),
-      k3: should
+      k3: should,
+      k4: should.date().description("ces"),
+      ar: should.array().description("数组"),
+      bo: should.boolean().description("布尔")
     }).required().description("测试对象").example({
       k1: '哈哈',
       k2: 4,
       k3: new Date()
     }),
-    should.example(1).description("页码"))
-  list(obj, pageNo, pageSize, date) {
-    return {obj, pageNo, pageSize, date}
+    should.example(1).description("页码"), null, should.date().description("日期"))
+  list(obj, pageNo, pageSize, date1, date2) {
+    return {obj, pageNo, pageSize, date1, date2}
   }
 
   list2(obj, date, test) {
